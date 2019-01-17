2deafc4884409a068efdfa1e10f165080a001871
liulei3 <liulei3@guahao.com>
2019-01-17 17:57:17 +0800

    Event Handling

--- main.js
+++ main.js
@@ -8,12 +8,23 @@ var app = new Vue({
         variants: [
             {
                 variantId: 2234,
-                variantColor: "green"
+                variantColor: "green",
+                variantImage: "./assets/vmSocks-green.jpeg"
             },
             {
                 variantId: 2235,
-                variantColor: "blue"
+                variantColor: "blue",
+                variantImage: "./assets/vmSocks-blue.jpeg"
             }
-        ]
+        ],
+        cart: 0
+    },
+    methods: {
+        addToCart: function() {
+            this.cart += 1
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        }
     }
 })
