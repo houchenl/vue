6efaa2fef882fe6bb5147dded45d04bccc19a849
liulei3 <liulei3@guahao.com>
2019-01-17 17:28:30 +0800

    v-show

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ var app = new Vue({
     data: {
         product: 'Socks',
         image: './assets/vmSocks-green.jpeg',
-        inStock: true,
-        inventory: 100
+        inStock: true
     }
 })
