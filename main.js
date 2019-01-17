44160dac2500423246a377e157f4dab54a7497a9
liulei3 <liulei3@guahao.com>
2019-01-17 17:25:35 +0800

    v-else-if

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var app = new Vue({
     data: {
         product: 'Socks',
         image: './assets/vmSocks-green.jpeg',
-        inStock: true
+        inStock: true,
+        inventory: 100
     }
 })
